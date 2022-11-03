13360e9e9da01b7a71d1d347b75e785b39318712
homile <ssanho2@naver.com>
2022-11-03 11:56:14 +0900

    fix:전역 스타일 크기 및 넓이 수정

--- ts-portfolio/src/assets/styles/GlobalStyle.ts
+++ ts-portfolio/src/assets/styles/GlobalStyle.ts
@@ -18,7 +18,7 @@ const GlobalStytle = createGlobalStyle`
   a{
     /* 텍스트의 밑줄 제거 */
     text-decoration: none;
-    color: var(--color-white);
+    color: #ffffff;
   }
 
   ul{
@@ -37,32 +37,31 @@ const GlobalStytle = createGlobalStyle`
     outline: none;
   }
 
-  /* Typography */
   h1{
-    font-size: var(--font-large);
-    font-weight: var(--weight-bold);
-    color: var(--color-black);
+    font-size: 48px;
+    font-weight: 700;
+    color: #000000;
     margin: 16px 0;
   }
 
   h2{
-    font-size: var(--font-medium);
-    font-weight: var(--weight-semi-bold);
-    color: var(--color-black);
+    font-size: 28px;
+    font-weight: 600;
+    color: #000000;
     margin: 8px 0;
   }
 
   h3{
-    font-size: var(--font-regular);
-    font-weight: var(--font-regular);
-    color: var(--color-black);
+    font-size: 18px;
+    font-weight: 400;
+    color: #000000;
     margin: 8px 0;
   }
 
   p{
-    font-size: var(--font-regular);
-    font-weight: var(--font-regular);
-    color: var(--color-black);
+    font-size: 18px;
+    font-weight: 400;
+    color: #000000;
     margin: 4px 0;  
   }
 

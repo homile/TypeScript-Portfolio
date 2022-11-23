5beb0850cf8081c0c327783f8838349cf29b232c
homile <ssanho2@naver.com>
2022-11-23 21:50:51 +0900

    feat:티스토리, 깃헙 로고 생성

--- ts-portfolio/src/components/button/SnsLink.tsx
+++ ts-portfolio/src/components/button/SnsLink.tsx
@@ -1,16 +1,81 @@
 import React from 'react';
 import styled from 'styled-components';
+import { github, blog } from '../../assets/img/sns/index';
 
-interface Props {
+interface dataType {
   title: string;
   color: string;
   img: string;
+  link: string;
 }
 
-const SnsLink = ({ title, color, img }: Props) => {
-  return <Container>{title}</Container>;
+const SnsLink = () => {
+  const snsData: dataType[] = [
+    {
+      title: 'Blog',
+      color: '#EA5220',
+      img: `${blog}`,
+      link: 'https://homile.tistory.com/',
+    },
+    {
+      title: 'Github',
+      color: '#000000',
+      img: `${github}`,
+      link: 'https://github.com/homile',
+    },
+  ];
+
+  return (
+    <Ul>
+      {snsData.map((sns, idx: number) => {
+        return (
+          <Li key={idx} color={sns.color}>
+            <a href={sns.link}>
+              <Title color={sns.color}>{sns.title}</Title>
+              <Logo src={sns.img} />
+            </a>
+          </Li>
+        );
+      })}
+    </Ul>
+  );
 };
 
 export default SnsLink;
 
-const Container = styled.div``;
+const Ul = styled.ul`
+  display: flex;
+`;
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
+  font-weight: ${(props) => props.theme.weightBold};
+  font-size: ${(props) => props.theme.fontRegular};
+  height: 2.75rem;
+  padding-right: 0.625rem;
+  margin-right: 8px;
+  cursor: pointer;
+  border: 1px solid ${(props) => props.theme.colorDarkWhite};
+  border-radius: ${(props) => props.theme.sizeBorderRadius};
+
+  &:hover {
+    border: 1px solid ${(props) => props.color};
+    border-radius: ${(props) => props.theme.sizeBorderRadius};
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Title = styled.span`
+  color: ${(props) => props.color};
+  padding: 0 0.625rem;
+`;
+
+const Logo = styled.img`
+  width: 2.25rem;
+  height: 2.25rem;
+`;

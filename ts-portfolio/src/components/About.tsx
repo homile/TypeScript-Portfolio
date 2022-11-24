ec71ff32520a5f21ad42f3b360262d0bc142670c
homile <ssanho2@naver.com>
2022-11-24 20:32:35 +0900

    feat:About 화면 구성

--- ts-portfolio/src/components/About.tsx
+++ ts-portfolio/src/components/About.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import Univ from '../assets/img/김포대학교.png';
-import highShool from '../assets/img/김포제일공고.png';
+import AboutCard from './cards/AboutCard';
+import Title from './ui/Title';
 
 const About = () => {
   return (
-    <AboutSection className="section">
+    <AboutSection>
+      <Title title="About Me" />
+      <AboutCard />
     </AboutSection>
   );
 };
 
 export default About;
 
-const AboutSection = styled.section``;
+const AboutSection = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 100%;
+  padding: 40px;
+  padding-top: 120px;
+  text-align: center;
+`;

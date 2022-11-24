8e1aff553a5c46db117370217e53143597620ca4
homile <ssanho2@naver.com>
2022-11-24 19:47:13 +0900

    feat:항목 별 제목 컴포넌트 생성

---
+++ ts-portfolio/src/components/ui/Title.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  title: string;
+}
+
+const Title = (props: Props) => {
+  return <H1>{props.title}</H1>;
+};
+
+export default Title;
+
+const H1 = styled.h1`
+  font-weight: ${(props) => props.theme.weightBold};
+  font-size: 36px;
+`;

fd2117418ad4b7116f27314ded2585175f5a0ffd
homile <ssanho2@naver.com>
2022-11-26 18:21:30 +0900

    fix:불필요 이미지 삭제 및 변수명 재설정

--- ts-portfolio/src/assets/img/skills/front/index.ts
+++ ts-portfolio/src/assets/img/skills/front/index.ts
@@ -3,17 +3,17 @@ import CSS from './CSS.png';
 import Javascript from './Javascript.png';
 import Typescript from './Typescript.png';
 import Next from './Next.png';
-import React from './React.png';
+import ReactJS from './React.png';
 import Redux from './Redux.png';
-import StyledComponents from './Styled-components.png';
+import StyledComponents from './StyledComponents.png';
 
 export {
   HTML,
   CSS,
   Javascript,
   Typescript,
+  ReactJS,
   Next,
-  React,
   Redux,
   StyledComponents,
 };

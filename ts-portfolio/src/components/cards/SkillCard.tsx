36dbfa2fc921a13fb101795cb4c42eec0a9fea1c
homile <ssanho2@naver.com>
2022-11-28 21:11:33 +0900

    feat:Skills 컴포넌트 구현 완료

--- ts-portfolio/src/components/cards/SkillCard.tsx
+++ ts-portfolio/src/components/cards/SkillCard.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import {
+  HTML,
+  CSS,
+  Javascript,
+  Typescript,
+  ReactJS,
+  Next,
+  Redux,
+  StyledComponents,
+} from '../../assets/img/skills/front/index';
+import { Java, Python, Spring } from '../../assets/img/skills/back/index';
+import {
+  AWS,
+  Firebase,
+  Git,
+  Github,
+  Jira,
+  Slack,
+  Notion,
+  Vercel,
+} from '../../assets/img/skills/deploy-etc/index';
+import { RootState } from '../../redux/store';
+
+const skillsData = [
+  [HTML, CSS, Javascript, Typescript, ReactJS, Next, Redux, StyledComponents],
+  [Java, Python, Spring, Firebase],
+  [AWS, Vercel],
+  [Git, Github, Notion, Jira, Slack],
+];
 
 const SkillCard = () => {
-  return (
-    <div>SkillCard</div>
-  )
-}
+  const selectSkill: number = useSelector(
+    (state: RootState) => state.skillReducer.skillSelected,
+  );
 
-export default SkillCard
+  return (
+    <CardContainer>
+      {skillsData[selectSkill].map((el, idx: number) => {
+        return (
+          <Card key={idx}>
+            <CardImg src={el}></CardImg>
+          </Card>
+        );
+      })}
+    </CardContainer>
+  );
+};
+
+export default SkillCard;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 1rem;
+  transition: all 0.3s linear;
+
+  &:hover{
+    transform: scale(1.2);
+  }
+`;
+
+const CardImg = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+`;

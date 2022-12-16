92c34e8c2cc20e37efff1d9d47d19ea2ec7905fe
homile <ssanho2@naver.com>
2022-12-16 20:39:43 +0900

    style:프로젝트 카드 디자인 수정중

--- ts-portfolio/src/components/cards/ProjectCard.tsx
+++ ts-portfolio/src/components/cards/ProjectCard.tsx
@@ -47,13 +47,34 @@ const ProjectCard = () => {
                 <Img src={el.img}></Img>
               </div>
               <div className="right">
-                <Contents>개발기간</Contents>
-                <Contents>설명</Contents>
-                <Contents>프론트엔드</Contents>
-                <Contents>백엔드</Contents>
-                <Contents>배포링크</Contents>
-                <Contents>코드</Contents>
-                <Contents>개발기간</Contents>
+                <Contents>
+                  <SpanTitle>개발기간</SpanTitle>
+                  <SpanDesc>{el.devPeriod}</SpanDesc>
+                </Contents>
+                <Contents>
+                  <SpanTitle>설명</SpanTitle>
+                  <SpanDesc>{el.desc}</SpanDesc>
+                </Contents>
+                <Contents>
+                  <SpanTitle>프론트엔드</SpanTitle>
+                  <SpanDesc>{el.front}</SpanDesc>
+                </Contents>
+                <Contents>
+                  <SpanTitle>백엔드</SpanTitle>
+                  <SpanDesc>{el.back}</SpanDesc>
+                </Contents>
+                <Contents>
+                  <SpanTitle>배포링크</SpanTitle>
+                  <ALink href={el.link}>배포링크 이동</ALink>
+                </Contents>
+                <Contents>
+                  <SpanTitle>코드</SpanTitle>
+                  <ALink href={el.code}>Github 이동</ALink>
+                </Contents>
+                <Contents>
+                  <SpanTitle>개발문서</SpanTitle>
+                  <ALink href={el.doc}>개발문서 이동</ALink>
+                </Contents>
               </div>
             </div>
           </Card>
@@ -73,6 +94,7 @@ const CardContainer = styled.div`
   .right {
     display: flex;
     flex-direction: column;
+    height: 530px;
   }
 
   .project {
@@ -82,10 +104,41 @@ const CardContainer = styled.div`
   }
 `;
 
-const Card = styled.div``;
+const Card = styled.div`
+  width: 1000px;
+  height: 677px;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 1.5rem;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+`;
 
-const ProjectName = styled.h2``;
+const ProjectName = styled.h2`
+  margin-bottom: 2rem;
+`;
 
-const Img = styled.img``;
+const Img = styled.img`
+  width: 440px;
+  height: 530px;
+  object-fit: fill;
+  border: 1px solid black;
+  border-radius: 10px;
+`;
 
-const Contents = styled.span``;
+const Contents = styled.div`
+  display: flex;
+  margin: 1rem;
+`;
+
+const SpanTitle = styled.span`
+  display: flex;
+  font-weight: bold;
+  width: 100px;
+`;
+
+const SpanDesc = styled.span``;
+
+const ALink = styled.a`
+  color: ${(props) => props.theme.colorBlack}; ;
+`;
